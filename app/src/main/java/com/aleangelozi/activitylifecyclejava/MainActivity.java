0590586c136effbc7baebec236f439bb41d7c89d
aleangelozi <aleangelozi@gmail.com>
2021-05-01 21:50:28 -0300

    Migration to View Binding.

--- app/src/main/java/com/aleangelozi/activitylifecyclejava/MainActivity.java
+++ app/src/main/java/com/aleangelozi/activitylifecyclejava/MainActivity.java
@@ -2,17 +2,24 @@ package com.aleangelozi.activitylifecyclejava;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.aleangelozi.activitylifecyclejava.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "Activity method";
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
         Log.d(TAG, "onCreate");
     }
 
